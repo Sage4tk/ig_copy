764a18a4deed8310a47402ca25d9e569d31de34b
timothy timbol <ttimbol08@gmail.com>
2022-04-06 18:07:56 +0400

    added media query for nav

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -10,34 +10,45 @@ import request from "./request.svg";
 
 //context
 import { useUser } from "../../context/AuthContext";
+import { url } from "inspector";
 
 const Nav:React.FC<any> = () => {
     //input text state
     const [inputText, setInputText] = useState("");
+    const { user } = useUser();
 
+    useEffect(() => {
+        console.log(user.displayName)
+    }, [user])
     //menu toggle
     const [menu, setMenu] = useState(false);
 
     return (
         <nav>
-            <h1>Instagram</h1>
-            <div className="nav-search">
-                <input placeholder="Search" value={inputText} onChange={(e) => setInputText(e.target.value)}/>
-                <img src={search} className="icon" alt="" />
-            </div>
-            <div className="nav-icons">
-                <img src={home} alt=""/>
-                <img src={add} alt="" />
-                <img src={request} alt="" />
-                <div className="nav-profile" onClick={() => {setMenu(!menu)}}>
-                    {menu && <DropDown />}
-                </div>   
+            <div className="nav-container">
+                <h1>Instagram</h1>
+                <div className="nav-search">
+                    <input placeholder="Search" value={inputText} onChange={(e) => setInputText(e.target.value)}/>
+                    <img src={search} className="icon" alt="" />
+                </div>
+                <div className="nav-icons">
+                    <img src={home} alt=""/>
+                    <img src={add} alt="" />
+                    <img src={request} alt="" />
+                    <div className="nav-profile" onClick={() => {setMenu(!menu)}} style={{backgroundImage:`url(${user.photoURL})`, backgroundSize:"cover"}}>
+                        {menu && <DropDown user={user} />}
+                    </div>   
+                </div>
             </div>
         </nav>
     )
 }
 
-const DropDown:React.FC<any> = () => {
+interface DropProps {
+    user: object
+}
+
+const DropDown:React.FC<DropProps> = ({ user }) => {
     //logout
     const { signOut } = useUser();
 
