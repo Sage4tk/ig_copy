6e4cef2ef4b30d907fe7c540b9173217602554f0
timothy timbol <ttimbol08@gmail.com>
2022-04-07 00:36:33 +0400

    started work on add post

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -10,20 +10,20 @@ import request from "./request.svg";
 
 //context
 import { useUser } from "../../context/AuthContext";
-import { url } from "inspector";
 
 const Nav:React.FC<any> = () => {
     //input text state
     const [inputText, setInputText] = useState("");
     const { user } = useUser();
 
-    useEffect(() => {
-        console.log(user.displayName)
-    }, [user])
     //menu toggle
     const [menu, setMenu] = useState(false);
 
+    //add post toggle
+    const [addPost, setAddPost] = useState(false);
+
     return (
+        <>
         <nav>
             <div className="nav-container">
                 <h1>Instagram</h1>
@@ -33,7 +33,7 @@ const Nav:React.FC<any> = () => {
                 </div>
                 <div className="nav-icons">
                     <img src={home} alt=""/>
-                    <img src={add} alt="" />
+                    <img src={add} alt="" onClick={()=> {setAddPost(!addPost)}}/>
                     <img src={request} alt="" />
                     <div className="nav-profile" onClick={() => {setMenu(!menu)}} style={{backgroundImage:`url(${user.photoURL})`, backgroundSize:"cover"}}>
                         {menu && <DropDown user={user} />}
@@ -41,6 +41,8 @@ const Nav:React.FC<any> = () => {
                 </div>
             </div>
         </nav>
+        <AddPost open={addPost} />
+        </>
     )
 }
 
@@ -64,6 +66,23 @@ const DropDown:React.FC<DropProps> = ({ user }) => {
             </div>
         </div>    
         </>
+    )
+}
+
+interface AddProps {
+    open: boolean
+}
+
+const AddPost:React.FC<AddProps> = ({ open }) => {
+
+    if (!open) return (null)
+
+    return (
+        <div className="add-container">
+            <div className="add-window">
+
+            </div>
+        </div>
     )
 }
 
