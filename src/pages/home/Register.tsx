34df60d78ad33369948efa9bd40a1ec2c7e1abd6
timothy timbol <ttimbol08@gmail.com>
2022-05-01 22:50:26 +0400

    added input checker and error display for set username on db

--- src/pages/home/Register.tsx
+++ src/pages/home/Register.tsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
+import { db } from "../../firebase";
+
 interface RegisterProps {
     register: boolean
 }
 
 const Register:React.FC<RegisterProps> = ({ register }) => {
+    //form handler
+    const [formHandler, setFormHandler] = useState<string>("");
+
+    //display error
+    const [errorInput, setErrorInput] = useState(false);
+
+    //submit string to db
+    const submitUsername = () => {
+        if ((/\s/g).test(formHandler) || formHandler.length < 3) {
+            setErrorInput(true)
+        }
+    }
+
     if (register) return (null);
 
     return (
@@ -12,8 +28,9 @@ const Register:React.FC<RegisterProps> = ({ register }) => {
                     <h1>Set a Username</h1>
                 </div>
                 <div className="setusername">
-                    <input type="text" name="username" placeholder="username"/>
-                    <button>Submit</button>
+                    <input type="text" name="username" placeholder="username" onChange={(e) => {setFormHandler(e.target.value)}}/>
+                    {errorInput && <p className="error">User name must contain more than 3 letters</p>}
+                    <button onClick={submitUsername}>Submit</button>
                 </div>
             </div>
         </div>
